494fbaa838ceeb55ede2356301b94ce45c8f9e1a
Chely Ho <adept_inept@yahoo.com>
2018-02-08 20:53:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,6 +51,22 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].values.flatten
 end
 
+holiday_hash = {
+:winter => {
+  :christmas => ["Lights", "Wreath"],
+  :new_years => ["Party Hats"]
+},
+:summer => {
+  :fourth_of_july => ["Fireworks", "BBQ"]
+},
+:fall => {
+  :thanksgiving => ["Turkey"]
+},
+:spring => {
+  :memorial_day => ["BBQ"]
+}
+}
+
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
