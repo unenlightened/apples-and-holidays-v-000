8f9e90c3c1d57854e23bee47fe053ada06ef9b35
Chely Ho <adept_inept@yahoo.com>
2018-02-08 21:36:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,10 @@ def all_supplies_in_holidays(holiday_hash)
 
   holiday_hash.each do |season, holiday|
     puts "#{string_cleaning(season)}:"
-
+      season.each do |holiday, supplies|
+        supplies_cleaned = []
+        supplies.each do |supply|
+      
 
 
   end
