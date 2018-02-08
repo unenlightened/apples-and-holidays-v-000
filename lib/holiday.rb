a3a4b7dd4a75574178e69898684a0c7e1d69a812
Chely Ho <adept_inept@yahoo.com>
2018-02-08 22:00:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,7 @@ def all_holidays_with_bbq(holiday_hash)
 
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday,supplies|
-      if supplies.find("BBQ")
+      if supplies
         match << holiday
       end
     end
